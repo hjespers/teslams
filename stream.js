b79e8039bb56c6aba84e51bd50fdd1c9afdf9ba0
Hans Jespersen <hans.jespersen@solacesystems.com>
2013-02-19 14:10:18 -0800

    major restructuring

--- stream.js
+++ stream.js
@@ -1,19 +1,13 @@
 var request = require('request');
 var fs = require('fs');
+var teslams = require('./teslams');
 
 // edit the credentials to contain your teslamotors.com login email and password
 var creds = { email: "youremail.com", password: "yourpassword" }
 
-function wake_up( vid ) {
-	request('https://portal.vn.teslamotors.com/vehicles/' + vid + '/command/wake_up', function (error, response, body) { 
-		var data = JSON.parse(body); 
-		console.log("\nWake up!");
-		console.log(data);
-	});
-}
-
 function get_stream( long_vid, password ) {
-	request( 
+	if ( long_vid != undefined ) {
+	   request( 
 		{ 
 			'uri': 'https://streaming.vn.teslamotors.com/stream/' + long_vid +'/?values=speed,odometer,soc,elevation,est_heading,est_lat,est_lng,power,shift_state', 
 			'method' : 'GET',
@@ -21,44 +15,48 @@ function get_stream( long_vid, password ) {
 		},  
 		function( error, response, body) {
 			if (!error && response.statusCode == 200) {
-                		console.log('Response BODY: ' + body);
-			        console.log('values=timestamp,speed,odometer,soc,elevation,est_heading,est_lat,est_lng,power,shift_state'); 
+			        console.log('timestamp,speed,odometer,soc,elevation,est_heading,est_lat,est_lng,power,shift_state\n'); 
         		} else {
                			console.log('Problem with request: status code = ' + response.statusCode);
 				console.log( error );
 				console.log( response );
 				console.log( body );
+				process.exit(1);
          		}	
 			get_stream( long_vid, password ); // keep calling again and again
 		}
-	).pipe(fs.createWriteStream('stream_output.txt', {'flags': 'a'} ));
+	   ).pipe(fs.createWriteStream('stream_output.txt', {'flags': 'a'} ));
+	} else {
+		console.log('Error: undefined vehicle_id');
+		process.exit(1);
+	}
 }
 
 //
 // Login, get cookies, and figure out the vehicle ID (vid) for subsequent queries
 //
-var tesla = request( { method: 'POST',
-	   url: 'https://portal.vn.teslamotors.com/login',
+var mytesla = request( { method: 'POST',
+	   url: teslams.portal + '/login',
 	   form:{
 		"user_session[email]": creds.email, 
 		"user_session[password]": creds.password
 	   }}, 
 	   function (error, response, body) {
 		if (!error) {
-			request('https://portal.vn.teslamotors.com/vehicles', function (error, response, body) 
+			request(teslams.portal + '/vehicles', function (error, response, body) 
 				  { 
 					var data = JSON.parse( body.substr(1, body.length - 2 ) ); 
 					console.log(data);
-					tesla.id = data.id;
-					tesla.vid = data.vehicle_id;
-					tesla.pw = data.tokens[0];
-					console.log("streaming output to file stream_output.txt");
-					if (tesla.pw == undefined ) {
+					mytesla.id = data.id;
+					mytesla.vid = data.vehicle_id;
+					mytesla.pw = data.tokens[0];
+					console.log('Streaming output for vehicle_id (' + mytesla.vid + ') to file stream_output.txt');
+					if (mytesla.pw == undefined ) {
 						console.log("Error: no streaming password token");
-						wake_up( tesla.id);
-						get_stream( tesla.vid, tesla.pw );
+						teslams.wake_up( mytesla.id);
+						get_stream( mytesla.vid, mytesla.pw );
 					} else {
-						get_stream( tesla.vid, tesla.pw );
+						get_stream( mytesla.vid, mytesla.pw );
 					}
 				  }
 			).pipe(fs.createWriteStream('stream_output.txt'));
