92727ced77c58a27207ef14b3971d52b617f7f3c
Dirk Hohndel <dirk@hohndel.org>
2013-08-01 12:53:02 -0700

    Move HTML source into a separate file
    Load the file and replace some MAGIC_ values with the correct data at runtime.
    With this there's an easy to edit and maintain 'map.html' file that gets used
    as the basis for the file served.
    
    Also extend the window for searching for timestamps to ten minutes (as I found
    two instances where there was a gap of more than five minutes in my data).
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- examples/visualize.js
+++ examples/visualize.js
@@ -36,6 +36,8 @@ if ( argv.help == true ) {
 var MongoClient = require('mongodb').MongoClient;
 var date = new Date();
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 var lastTime = 0;
 var started = false;
 http.createServer(function(req, res) {
@@ -47,8 +49,8 @@ http.createServer(function(req, res) {
 		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
 			if(!err) {
 				collection = db.collection("tesla_stream");
-				// get no more than 5 minutes or 240 samples, whichever is smaller
-				collection.find({"ts": {$gt: lastTime, $lte: lastTime + 300000}}, { limit: 240}).toArray(function(err,docs) {
+				// get no more than 10 minutes or 240 samples, whichever is smaller
+				collection.find({"ts": {$gt: lastTime, $lte: lastTime + 600000}}, { limit: 240}).toArray(function(err,docs) {
 					res.setHeader("Content-Type", "application/json"); 
 					res.write("[", "utf-8");
 					var comma = "";
@@ -79,65 +81,14 @@ http.createServer(function(req, res) {
 					var record = doc.record;
 					var vals = record.toString().replace(",,",",0,").split(/[,\n\r]/);
 					if (lastTime == 0) { lastTime = +vals[0]; }
-					//
-					// this creates the page displayed in the browser;
-					// I tried to make this code somewhat dense without sacrificing too much readability either
-					// here in the sources or when debugging it in the browser; that's why it is all sent as
-					// one long string that is assembled here across many lines of code.
-					// the getColor function is just a little piece of mathermatical magic that matches a speed
-					// to a wavelength and then does a crude wavelength to RGB conversion
-					//
 					res.setHeader("Content-Type", "text/html"); 
-					res.end("<!DOCTYPE html><html><head>\n" +
-						"<meta name='viewport' content='initial-scale=1.0, user-scalable=no' />\n" +
-						"<style type='text/css'> html { height: 100% } body { height: 100%; margin: 0; padding: 0 } #map-canvas { height: 90% } </style>\n" +
-						"<script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>\n" +
-						"<script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&sensor=true'></script>\n" +
-						"<script type='text/javascript'>\n" +
-						"var myLatlng; var map; var marker; var infoWindow;\n" +
-						"function hex(num) { var s = '#0' + num.toString(16) + ''; return s.substring(s.length - 2); }\n" +
-						"function getColor(speed) { \n" +
-						"	var wl = speed*2+440; if(wl>640){wl=640;}\n" +
-						"	var r=Math.round(35+(wl-510)*3.14); if(r<55){r=55} if(r>255){r=255}\n" +
-						"	var b=35+(510-wl)*11;if(b<55){b=55;} if(b>255){b=255}\n" +
-						"	var g=(wl<580) ? Math.round(35 + (wl - 440) * 4.4) : Math.round(35 + (640 - wl) * 3.66); if(g<55){g=55} if(g>255){g=255}\n" +
-						"	return '#'+hex(r)+hex(g)+hex(b);\n" +
-						"}\n" +
-						"function getMore() { \n" +
-						"	$.getJSON('http://localhost:" + argv.port + "/update', function (d, textStatus, jqXHR) { \n" +
-						"		if (d.length == 0) { return; }\n" +
-						"		for (var i = 0; i < d.length; i++) { \n" +
-						"			var nextLatlng = new google.maps.LatLng(d[i][6],d[i][7]);\n" +
-						"			var points = [myLatlng,nextLatlng];\n" +
-						"			var color = getColor(d[i][1]);\n" +
-						"			var opacity = (d[i][0] - getMore.lastTimestamp > 5000) ? 0.1 : 0.9;\n" +
-						"			var path = new google.maps.Polyline({path:points,strokeColor:color,strokeOpacity:opacity,strokeWeight:4});\n" +
-						"			path.setMap(map);\n" +
-						"			if (i == d.length - 1) {\n" +
-						"				var cStr = '<div id=\"content\">';\n" +
-						"				marker.setPosition(nextLatlng);\n" +
-						"				if (d[i][9] == 'D' || d[i][9] == 'R'){cStr+='going '+d[i][1]+' mph'}\n" +
-						"				else if (d[i][8] < 0){cStr+='parked and charging ~ '+(-d[i][8])+'kW'}\n" +
-						"				else {cStr+='parked'}\n" +
-						"				cStr+='<br>odometer '+d[i][2]+'<br>range '+d[i][10]+'</div>';\n" +
-						"				infoWindow.setContent(cStr);\n" +
-						"			}\n" +
-						"			myLatlng = nextLatlng;\n" +
-						"		}\n" +
-						"	}); }\n" +
-						"function initialize() { \n" +
-						"	myLatlng = new google.maps.LatLng(" + vals[6] + "," + vals[7] + ");\n" +
-						"	var mapOptions = { center: myLatlng, zoom: 16, mapTypeId: google.maps.MapTypeId.ROADMAP };\n" + 
-						"	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);\n" + 
-						"	marker = new google.maps.Marker({ position: myLatlng, map: map, title: 'Tesla Model S'});\n" +
-						"	var cStr = \'<div id=\"content\">going " + vals[1] + " mph<br>odometer " + vals[2] + "<br>range " + vals[10] + "</div>\';\n" +
-						"	infoWindow = new google.maps.InfoWindow({content: cStr});\n" +
-						"	infoWindow.open(map, marker);\n" +
-						"	setInterval(getMore, 2000);\n" +
-						"}\n" + 
-						"google.maps.event.addDomListener(window, 'load', initialize);\n" + 
-						"</script>\n" + 
-						"</head> <body> <div id='map-canvas'/> </body> </html>\n", "utf-8");
+					fs.readFile("./map.html", "utf-8", function(err, data) {
+						if (err) throw err;
+						var response = data.replace("MAGIC_APIKEY", apiKey)
+									.replace("MAGIC_UPDATE_URL", "http://localhost:" + argv.port + "/update")
+									.replace("MAGIC_FIRST_LOC", vals[6] + "," + vals[7]);
+						res.end(response, "utf-8");
+					});
 				});
 				db.close();
 				started = true;
