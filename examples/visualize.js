be550295e1076067e0ade3075ecdde6b265e0d16
Dirk Hohndel <dirk@hohndel.org>
2013-08-22 08:52:03 -0700

    Make visualize.js more readable
    This is almost exclusively a change of indentation levels (by modifying the way
    the code checks for an error when connecting to the database). There are a
    couple of minor changes on when we throw an error versus just report it.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- examples/visualize.js
+++ examples/visualize.js
@@ -50,6 +50,7 @@ var to;
 
 var capacity;
 MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
+	// this is the first time we connect - if we get an error, just throw it
 	if(err) throw(err);
 	var collectionA = db.collection("tesla_aux");
 	// get the last stored entry that describes the vehicles
@@ -88,38 +89,40 @@ http.createServer(function(req, res) {
 		if (!started)
 			return;
 		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
-			if(!err) {
-				collection = db.collection("tesla_stream");
-				// get no more than 10 minutes or 240 samples, whichever is smaller
-				var endTime = +lastTime + 600000;
-				if (to && +endTime > +to)
-					endTime = +to;
-				collection.find({"ts": {"$gt": +lastTime, "$lte": +endTime}}).toArray(function(err,docs) {
-					if (argv.verbose) console.log("got datasets:", docs.length);
-					if (docs.length == 0) {
-						// create one dummy entry so the map app knows the last time we looked at
-						docs = [ { "ts": +endTime, "record": [ +lastTime+"" ,"0","0","0","0","0","0","0","0","0","0","0"]} ];
-						lastTime = +endTime; // skip this segment
-					}
-					res.setHeader("Content-Type", "application/json"); 
-					res.write("[", "utf-8");
-					var comma = "";
-					docs.forEach(function(doc) {
-						// the tesla streaming service replaces a few items with "" when the car is off
-						// the reg exp below replaces the two that are numerical with 0 (the shift_state stays unchanged)
-						var vals = doc.record.toString().replace(",,",",0,").split(",");
-						res.write(comma + JSON.stringify(vals), "utf-8");
-						lastTime = doc.ts;
-						comma = ",";
-					});
-					res.end("]", "utf-8");
-					if (!argv.silent) {
-						var showTime = new Date(lastTime);
-						console.log("last timestamp:", lastTime, showTime.toString());
-					}
-					db.close();
-				});
+			if(err) {
+				console.log('error connecting to database:', err);
+				return;
 			}
+			collection = db.collection("tesla_stream");
+			// get no more than 10 minutes or 240 samples, whichever is smaller
+			var endTime = +lastTime + 600000;
+			if (to && +endTime > +to)
+				endTime = +to;
+			collection.find({"ts": {"$gt": +lastTime, "$lte": +endTime}}).toArray(function(err,docs) {
+				if (argv.verbose) console.log("got datasets:", docs.length);
+				if (docs.length == 0) {
+					// create one dummy entry so the map app knows the last time we looked at
+					docs = [ { "ts": +endTime, "record": [ +lastTime+"" ,"0","0","0","0","0","0","0","0","0","0","0"]} ];
+					lastTime = +endTime; // skip this segment
+				}
+				res.setHeader("Content-Type", "application/json"); 
+				res.write("[", "utf-8");
+				var comma = "";
+				docs.forEach(function(doc) {
+					// the tesla streaming service replaces a few items with "" when the car is off
+					// the reg exp below replaces the two that are numerical with 0 (the shift_state stays unchanged)
+					var vals = doc.record.toString().replace(",,",",0,").split(",");
+					res.write(comma + JSON.stringify(vals), "utf-8");
+					lastTime = doc.ts;
+					comma = ",";
+				});
+				res.end("]", "utf-8");
+				if (!argv.silent) {
+					var showTime = new Date(lastTime);
+					console.log("last timestamp:", lastTime, showTime.toString());
+				}
+				db.close();
+			});
 		});
 	} else if (path == "/") {
 		var fromParts = (parsedUrl.query.from + "-0").split("-");
@@ -129,8 +132,10 @@ http.createServer(function(req, res) {
 		if (toParts[5])
 			to = new Date(toParts[0], toParts[1] - 1, toParts[2], toParts[3], toParts[4], toParts[5]);
 		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
-			if (argv.verbose) console.log("connected to db");
-			if(err) throw err;
+			if(err) {
+				console.log('error connecting to database:', err);
+				return;
+			}
 			collection = db.collection("tesla_stream");
 			var startTime = (from) ? from : date.getTime() - argv.replay * 60 * 1000; // go back 'replay' minutes
 			var searchString;
@@ -180,66 +185,68 @@ http.createServer(function(req, res) {
 		var gMaxE = -1000, gMaxS = -1000;
 		var gMinE = 1000, gMinS = 1000;
 		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
-			if(!err) {
-				res.setHeader("Content-Type", "text/html");
-				collection = db.collection("tesla_stream");
-				collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
-					docs.forEach(function(doc) {
-						if (firstDate == 0) firstDate = lastDate = doc.ts;
-						if (doc.ts >= lastDate) {
-							var vals = doc.record.toString().replace(",,",",0,").split(",");
-							if (doc.ts > lastDate + increment) {
-								if (maxE != -1000) {
-									outputE += comma + "[" + (+lastDate + halfIncrement) + "," + maxE + "]";
-									outputE += ",[" + (+lastDate + increment) + "," + minE + "]";
-								}
-								if (maxS != -1000)
-									outputS += comma + "[" + (+lastDate + halfIncrement) + "," + (+maxS + +minS) / 2 + "]";
-								if (maxSOC != -1000)
-									outputSOC += comma + "[" + (+lastDate + halfIncrement)  + "," + (+maxSOC + +minSOC) / 2 + "]";
-								comma = ",";
-								lastDate = doc.ts;
-								if (+maxE > +gMaxE) gMaxE = maxE;
-								if (+minE < +gMinE) gMinE = minE;
-								if (+maxS > +gMaxS) gMaxS = maxS;
-								maxE = maxS = maxSOC = -1000;
-								minE = minS = minSOC = 1000;
-							}
-							if (+vals[8] > +maxE) maxE = vals[8];
-							if (+vals[8] < +minE) minE = vals[8];
-							if (+vals[1] > +maxS) maxS = vals[1];
-							if (+vals[1] < +minS) minS = vals[1];
-							if (+vals[3] > +maxSOC) maxSOC = vals[3];
-							if (+vals[3] < +minSOC) minSOC = vals[3];
-						}
-					});
-					db.close();
-					fs.readFile("./energy.html", "utf-8", function(err, data) {
-						if (err) throw err;
-						var fD = new Date(firstDate);
-						var startDate = (fD.getMonth() + 1) + "/" + fD.getDate() + "/" + fD.getFullYear();
-						gMinE = +gMinE - 10;
-						gMaxE = +gMaxE + 10;
-						if (2 * gMaxS > +gMaxE) {
-							gMaxS = +gMaxS + 5;
-							gMaxE = 2 * gMaxS;
-						} else {
-							gMaxS = gMaxE / 2;
-						}
-						gMinS = gMinE / 2;
-						var response = data.replace("MAGIC_ENERGY", outputE)
-									.replace("MAGIC_SPEED", outputS)
-									.replace("MAGIC_SOC", outputSOC)
-									.replace("MAGIC_START", startDate)
-									.replace("MAGIC_MAX_ENG", gMaxE)
-									.replace("MAGIC_MIN_ENG", gMinE)
-									.replace("MAGIC_MAX_SPD", gMaxS)
-									.replace("MAGIC_MIN_SPD", gMinS);
-						res.end(response, "utf-8");
-						if (argv.verbose) console.log("delivered", outputSOC.length,"records and", response.length, "bytes");
-					});
-				});
+			if(err) {
+				console.log('error connecting to database:', err);
+				return;
 			}
+			res.setHeader("Content-Type", "text/html");
+			collection = db.collection("tesla_stream");
+			collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
+				docs.forEach(function(doc) {
+					if (firstDate == 0) firstDate = lastDate = doc.ts;
+					if (doc.ts >= lastDate) {
+						var vals = doc.record.toString().replace(",,",",0,").split(",");
+						if (doc.ts > lastDate + increment) {
+							if (maxE != -1000) {
+								outputE += comma + "[" + (+lastDate + halfIncrement) + "," + maxE + "]";
+								outputE += ",[" + (+lastDate + increment) + "," + minE + "]";
+							}
+							if (maxS != -1000)
+								outputS += comma + "[" + (+lastDate + halfIncrement) + "," + (+maxS + +minS) / 2 + "]";
+							if (maxSOC != -1000)
+								outputSOC += comma + "[" + (+lastDate + halfIncrement)  + "," + (+maxSOC + +minSOC) / 2 + "]";
+							comma = ",";
+							lastDate = doc.ts;
+							if (+maxE > +gMaxE) gMaxE = maxE;
+							if (+minE < +gMinE) gMinE = minE;
+							if (+maxS > +gMaxS) gMaxS = maxS;
+							maxE = maxS = maxSOC = -1000;
+							minE = minS = minSOC = 1000;
+						}
+						if (+vals[8] > +maxE) maxE = vals[8];
+						if (+vals[8] < +minE) minE = vals[8];
+						if (+vals[1] > +maxS) maxS = vals[1];
+						if (+vals[1] < +minS) minS = vals[1];
+						if (+vals[3] > +maxSOC) maxSOC = vals[3];
+						if (+vals[3] < +minSOC) minSOC = vals[3];
+					}
+				});
+				db.close();
+				fs.readFile("./energy.html", "utf-8", function(err, data) {
+					if (err) throw err;
+					var fD = new Date(firstDate);
+					var startDate = (fD.getMonth() + 1) + "/" + fD.getDate() + "/" + fD.getFullYear();
+					gMinE = +gMinE - 10;
+					gMaxE = +gMaxE + 10;
+					if (2 * gMaxS > +gMaxE) {
+						gMaxS = +gMaxS + 5;
+						gMaxE = 2 * gMaxS;
+					} else {
+						gMaxS = gMaxE / 2;
+					}
+					gMinS = gMinE / 2;
+					var response = data.replace("MAGIC_ENERGY", outputE)
+								.replace("MAGIC_SPEED", outputS)
+								.replace("MAGIC_SOC", outputSOC)
+								.replace("MAGIC_START", startDate)
+								.replace("MAGIC_MAX_ENG", gMaxE)
+								.replace("MAGIC_MIN_ENG", gMinE)
+								.replace("MAGIC_MAX_SPD", gMaxS)
+								.replace("MAGIC_MIN_SPD", gMinS);
+					res.end(response, "utf-8");
+					if (argv.verbose) console.log("delivered", outputSOC.length,"records and", response.length, "bytes");
+				});
+			});
 		});
 	} else if (path == "/stats") {
 		if (!parsedUrl.query.to || !parsedUrl.query.from) {
@@ -253,106 +260,108 @@ http.createServer(function(req, res) {
 		var outputD = "", outputC = "", outputA = "", comma = "", commaD = "", firstDate = 0, lastDay = 0, lastDate = 0;
 		var startOdo = 0, charge = 0, minSOC = 101, maxSOC = -1, increment = 0;
 		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
-			if(!err) {
-				var vals = [];
-				res.setHeader("Content-Type", "text/html");
-				collection = db.collection("tesla_stream");
-				collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
-					docs.forEach(function(doc) {
-						var day = new Date(doc.ts).getDay();
-						vals = doc.record.toString().replace(",,",",0,").split(",");
-						if (firstDate == 0) {
-							firstDate = doc.ts;
+			if(err) {
+				console.log('error connecting to database:', err);
+				return;
+			}
+			var vals = [];
+			res.setHeader("Content-Type", "text/html");
+			collection = db.collection("tesla_stream");
+			collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
+				docs.forEach(function(doc) {
+					var day = new Date(doc.ts).getDay();
+					vals = doc.record.toString().replace(",,",",0,").split(",");
+					if (firstDate == 0) {
+						firstDate = doc.ts;
+						lastDay = day;
+						startOdo = vals[2];
+						minSOC = 101;
+						maxSOC = -1;
+					}
+					if (doc.ts > lastDate) { // we don't want to go back in time
+						if (day == lastDay) {
+							// still the same day - accumulate stats for charging
+							// this is crude - it would be much better to get this from
+							// the aux databased and use the actual charge info
+							if (vals[9] != 'R' && vals[9] != 'D' && vals[8] < 0) {
+								if (vals[3] < minSOC) minSOC = vals[3];
+								if (vals[3] > maxSOC) maxSOC = vals[3];
+								increment = maxSOC - minSOC;
+							} else {
+								if (increment > 0) {
+									charge += increment * capacity / 100;
+									increment = 0;
+									minSOC = 101;
+									maxSOC = -1;
+								}
+							}
+						} else {
 							lastDay = day;
+							var dist = +vals[2] - startOdo;
+							var ts = new Date(lastDate);
+							// in order for lines and bars to line up nicely it looks better if the
+							// bars start at midnight, but the lines are setup to have their values
+							// at midday
+							var midnight = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 0, 0, 0);
+							var midday = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 12, 0, 0);
+							charge += increment;
+							outputD += comma + "[" + +midnight  + "," + dist + "]";
+							outputC += comma + "[" + +midnight  + "," + charge + "]";
+							if (dist > 0) {
+								outputA += commaD + "[" + +midday  + "," + 1000 * charge / dist + "]";
+								commaD = ",";
+							}
 							startOdo = vals[2];
+							charge = 0;
 							minSOC = 101;
 							maxSOC = -1;
+							comma = ",";
 						}
-						if (doc.ts > lastDate) { // we don't want to go back in time
-							if (day == lastDay) {
-								// still the same day - accumulate stats for charging
-								// this is crude - it would be much better to get this from
-								// the aux databased and use the actual charge info
-								if (vals[9] != 'R' && vals[9] != 'D' && vals[8] < 0) {
-									if (vals[3] < minSOC) minSOC = vals[3];
-									if (vals[3] > maxSOC) maxSOC = vals[3];
-									increment = maxSOC - minSOC;
-								} else {
-									if (increment > 0) {
-										charge += increment * capacity / 100;
-										increment = 0;
-										minSOC = 101;
-										maxSOC = -1;
-									}
-								}
-							} else {
-								lastDay = day;
-								var dist = +vals[2] - startOdo;
-								var ts = new Date(lastDate);
-								// in order for lines and bars to line up nicely it looks better if the
-								// bars start at midnight, but the lines are setup to have their values
-								// at midday
-								var midnight = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 0, 0, 0);
-								var midday = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 12, 0, 0);
-								charge += increment;
-								outputD += comma + "[" + +midnight  + "," + dist + "]";
-								outputC += comma + "[" + +midnight  + "," + charge + "]";
-								if (dist > 0) {
-									outputA += commaD + "[" + +midday  + "," + 1000 * charge / dist + "]";
-									commaD = ",";
-								}
-								startOdo = vals[2];
-								charge = 0;
-								minSOC = 101;
-								maxSOC = -1;
-								comma = ",";
-							}
-							lastDate = doc.ts;
+						lastDate = doc.ts;
+					}
+				});
+				// we still need to add the last day
+				var dist = +vals[2] - startOdo;
+				var ts = new Date(lastDate);
+				var midnight = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 0, 0, 0);
+				var midday = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 12, 0, 0);
+				charge += increment;
+				outputD += comma + "[" + +midnight  + "," + dist + "]";
+				outputC += comma + "[" + +midnight  + "," + charge + "]";
+				if (dist > 0) {
+					outputA += commaD + "[" + +midday  + "," + 1000 * charge / dist + "]";
+					commaD = ",";
+				}
+				collection = db.collection("tesla_aux");
+				var maxAmp = 0, maxVolt = 0, maxMph = 0;
+				collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
+					if (argv.verbose) console.log("Found " + docs.length + " entries in aux DB");
+					docs.forEach(function(doc) {
+						if(typeof doc.chargeState !== 'undefined') {
+							if (doc.chargeState.charger_voltage > maxVolt)
+								maxVolt = doc.chargeState.charger_voltage;
+							if (doc.chargeState.charger_actual_current > maxAmp)
+								maxAmp = doc.chargeState.charger_actual_current;
+							if (doc.chargeState.charge_rate > maxMph)
+								maxMph = doc.chargeState.charge_rate;
 						}
 					});
-					// we still need to add the last day
-					var dist = +vals[2] - startOdo;
-					var ts = new Date(lastDate);
-					var midnight = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 0, 0, 0);
-					var midday = new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), 12, 0, 0);
-					charge += increment;
-					outputD += comma + "[" + +midnight  + "," + dist + "]";
-					outputC += comma + "[" + +midnight  + "," + charge + "]";
-					if (dist > 0) {
-						outputA += commaD + "[" + +midday  + "," + 1000 * charge / dist + "]";
-						commaD = ",";
-					}
-					collection = db.collection("tesla_aux");
-					var maxAmp = 0, maxVolt = 0, maxMph = 0;
-					collection.find({"ts": {$gte: +from, $lte: +to}}).toArray(function(err,docs) {
-						if (argv.verbose) console.log("Found " + docs.length + " entries in aux DB");
-						docs.forEach(function(doc) {
-							if(typeof doc.chargeState !== 'undefined') {
-								if (doc.chargeState.charger_voltage > maxVolt)
-									maxVolt = doc.chargeState.charger_voltage;
-								if (doc.chargeState.charger_actual_current > maxAmp)
-									maxAmp = doc.chargeState.charger_actual_current;
-								if (doc.chargeState.charge_rate > maxMph)
-									maxMph = doc.chargeState.charge_rate;
-							}
-						});
-						db.close();
-						fs.readFile("./stats.html", "utf-8", function(err, data) {
-							if (err) throw err;
-							var fD = new Date(firstDate);
-							var startDate = (fD.getMonth() + 1) + "/" + fD.getDate() + "/" + fD.getFullYear();
-							var response = data.replace("MAGIC_DISTANCE", outputD)
-									.replace("MAGIC_CHARGE", outputC)
-									.replace("MAGIC_AVERAGE", outputA)
-									.replace("MAGIC_START", startDate)
-									.replace("MAGIC_MAX_VOLT", maxVolt)
-									.replace("MAGIC_MAX_AMP", maxAmp)
-									.replace("MAGIC_MAX_MPH", maxMph);
-							res.end(response, "utf-8");
-						});
+					db.close();
+					fs.readFile("./stats.html", "utf-8", function(err, data) {
+						if (err) throw err;
+						var fD = new Date(firstDate);
+						var startDate = (fD.getMonth() + 1) + "/" + fD.getDate() + "/" + fD.getFullYear();
+						var response = data.replace("MAGIC_DISTANCE", outputD)
+								.replace("MAGIC_CHARGE", outputC)
+								.replace("MAGIC_AVERAGE", outputA)
+								.replace("MAGIC_START", startDate)
+								.replace("MAGIC_MAX_VOLT", maxVolt)
+								.replace("MAGIC_MAX_AMP", maxAmp)
+								.replace("MAGIC_MAX_MPH", maxMph);
+						res.end(response, "utf-8");
 					});
 				});
-			}
+			});
 		});
 	} else { // let's try to just deliver that file - but to avoid attacks, only the ones we know about
 		if (argv.verbose) console.log("request for", req.url);
