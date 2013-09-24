a928dad0a5865f5aecff9e33300401c5b1a31ea5
Hans Jespersen <hans.jespersen@gmail.com>
2013-09-23 20:20:09 -0700

    even more readable example

--- examples/examplestream.js
+++ examples/examplestream.js
@@ -29,18 +29,15 @@ try {
 //Note that the password used for the REST call to vehicles() is different than 
 //   the password used in the streaming API call to stream() even though the email is the same
 teslams.vehicles( { email: creds.email, password: creds.password }, function ( vehicles ) {
-	if (vehicles == undefined) {
-		console.log("Error: Undefined vehicle");
-	} else if (vehicles.tokens[0] == undefined) {
+    if (vehicles == undefined) {
+        console.log("Error: Undefined vehicle");
+    } else if (vehicles.tokens[0] == undefined) {
         console.log("Error: Undefined token, call wakeup first using 'teslacmd -w'");
     } else {
-		teslams.stream( {
-            email: creds.email,
-            password: vehicles.tokens[0], 
-            vehicle_id: vehicles.vehicle_id
-        }, function (error, response, body) {
-            console.log( body);
-        });
-	}
-  }
-);
+	console.log('Calling Streaming API, expect to wait up to 2 minutes for full response...');
+	var params = { email: creds.email, password: vehicles.tokens[0], vehicle_id: vehicles.vehicle_id };
+        teslams.stream( params, function (error, response, body) { 
+		console.log( body); 
+	});
+    }
+});
