e30cbc3cb2d6d038195fc49e97f6acf1c6c02696
Dirk Hohndel <dirk@hohndel.org>
2013-09-25 09:53:45 -0700

    Add German Fahrtenbuch as a way to visualize stored trips
    I'm planning to do an English / imperial version as well - but there are actual
    users of this in Germany so I went ahead with a German version, first.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- examples/visualize/visualize.js
+++ examples/visualize/visualize.js
@@ -70,6 +70,12 @@ fs.readFile(__dirname + "/otherfiles/nav.html", "utf-8", function(err, data) {
 	nav = data;
 });
 
+// shorthand to get leading zero when showing minutes
+function lZ(mins) {
+	var zmins = '0' + mins;
+	return zmins.substr(zmins.length - 2);
+}
+
 function makeDate(string, offset) {
 	var args = string.split('-');
 	var date = new Date(args[0], args[1]-1, args[2], args[3], args[4], args[5]);
@@ -738,6 +744,55 @@ app.get('/trip', function(req, res) {
 	fs.readFile(__dirname + "/trip.html", "utf-8", function(err, data) {
 		if (err) throw err;
 		res.end(data.replace("MAGIC_NAV", nav), "utf-8");
+	});
+});
+
+app.get('/fahrtenbuch', function(req, res) {
+	var path = req.path;
+	var dates = new parseDates(req.query.from, req.query.to);
+	var from = makeDate(dates.fromQ);
+	var to = makeDate(dates.toQ);
+	res.setHeader("Content-Type", "text/html");
+	fs.readFile(__dirname + "/fahrtenbuch.html", "utf-8", function(err, data) {
+		if (err) throw err;
+		var table = "<thead><tr><th colspan=9 id='title'>Fahrtenbuch</th></tr>\n";
+		table += "<tr><th colspan=6 class='left' id='Fahrer'>Fahrer: Tesla Fahrer</th><th colspan=3 class='left'>Abgabedatum:</th></tr>\n";
+		table += "<tr><th colspan=2>Abfahrt</th><th colspan=2>Ankunft</th>";
+		table += "<th rowspan=2>Wegstrecke</th><th rowspan=2>Reisezweck</th><th rowspan=2>Auto<br>Kennzeichen</th>";
+		table += "<th rowspan=2>KM Stand am<br>Zielort</th><th rowspan=2>Unterschrift</th></tr>";
+		table += "<tr><th>Datum</th><th>Zeit</th><th>Datum</th><th>Zeit</th></tr></thead>";
+		MongoClient.connect("mongodb://127.0.0.1:27017/" + argv.db, function(err, db) {
+			if(err) {
+				console.log('error connecting to database:', err);
+				return;
+			}
+			collection = db.collection("trip_data");
+			// strangely the timestamps end up in the database as strings
+			var searchString = {$and: [ {'from': {$gte: ""+from.getTime()}}, {'to': {$lte: ""+to.getTime()}} ] };
+			collection.find(searchString).toArray(function(err,docs) {
+				if (argv.verbose) console.log("got datasets:", docs.length);
+				table += "<tbody>\n";
+				docs.forEach(function(doc) {
+					table += "<tr>";
+					var depart = new Date(+doc.from);
+					var arrive = new Date(+doc.to);
+					table += "<td>" + depart.getDate() + "." + (depart.getMonth() + 1) + "." + (1900 + depart.getYear()) +"</td>";
+					table += "<td>" + depart.getHours() + ":" + lZ(depart.getMinutes())  + "</td>";
+					table += "<td>" + arrive.getDate() + "." + (arrive.getMonth() + 1) + "." + (1900 + arrive.getYear()) +"</td>";
+					table += "<td>" + arrive.getHours() + ":" + lZ(arrive.getMinutes())  + "</td>";
+					table += "<td>" + (1.609 * parseFloat(doc.dist)).toFixed(1) + "km</td>";
+					table += "<td>" + doc.name + "</td>";
+					table += "<td></td>";
+					table += "<td>" + (1.609 * parseFloat(doc.odo)).toFixed(1) + "</td>";
+					table += "<td></td>";
+					table += "</tr>\n";
+				});
+				table += "</tbody>\n";
+				db.close();
+				res.end(data.replace("MAGIC_NAV", nav)
+						.replace("MAGIC_TRIP_TABLE", table), "utf-8");
+			});
+		});
 	});
 });
 
