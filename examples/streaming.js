e31d5efc75d868a8bf2357cc506bedc5fa576107
Hans Jespersen <hans.jespersen@gmail.com>
2013-10-04 14:23:48 -0700

    Added sleep support, request counters, and throttling
    new -z flag to avoid waking a sleeping car
    added request per minute counters and throttling for both REST API and
    Streaming URL

--- examples/streaming.js
+++ examples/streaming.js
@@ -13,7 +13,8 @@ function argchecker( argv ) {
 	if (argv.db == true) throw 'MongoDB database name is unspecified. Use -d dbname or --db dbname';
 }
 
-var usage = 'Usage: $0 -u <username> -p <password> [--file <filename>] [--db <MongoDB database>] [--silent] \n' +
+var usage = 'Usage: $0 -u <username> -p <password> [-sz] [--file <filename> || --db <MongoDB database>] \n' +
+	'	[--values <value list>] [--maxrpm <#num>] \n' +
 	'# if --db <MongoDB database> argument is given, store data in MongoDB, otherwise in a flat file';
 
 var p_url = 'https://portal.vn.teslamotors.com/vehicles/';
@@ -22,6 +23,10 @@ var collectionS, collectionA;
 var firstTime = true;
 var MongoClient;
 var stream;
+var last = 0; // datetime for checking request rates
+var rpm = 0; // REST API Request Per Minute counter
+var slast = 0; // datetime for checking streaming request rates
+var srpm = 0; // Streaming URL Request Per Minute counter
 
 var argv = require('optimist')
 	.usage(usage)
@@ -34,10 +39,15 @@ var argv = require('optimist')
 	.describe('d', 'MongoDB database name')
 	.alias('s', 'silent')
 	.describe('s', 'Silent mode: no output to console')
-	.boolean(['s'])
+	.alias('z', 'zzz')
+	.describe('z', 'enable sleep mode checking')
+	.boolean(['s', 'z'])
 	.alias('f', 'file')
 	.describe('f', 'Comma Separated Values (CSV) output file. Defaults to streaming.out')
 	.default('f', 'streaming.out')
+	.alias('r', 'maxrpm')
+	.describe('r', 'Maximum number of requests per minute')
+	.default('r', 6)
 	.alias('v', 'values')
 	.describe('v', 'List of values to collect')
 	.default('v', 'speed,odometer,soc,elevation,est_heading,est_lat,est_lng,power,shift_state,range,est_range')
@@ -74,73 +84,72 @@ function tsla_poll( vid, long_vid, token ) {
 		console.log('Error: undefined vehicle_id (' + long_vid +') or token (' + token +')' );
 		console.log('Exiting...');
 		process.exit(1);
-	} else {
-		request(
-		{
-			'uri': s_url + long_vid +'/?values=' + argv.values,
+	} 
+	now = new Date().getTime();
+	if ( now - slast < 60000) { // last streaming request was less than 1 minute ago
+		ulog( srpm + ' of ' + argv.maxrpm + ' Stream requests since ' + slast);
+		if ( now - slast < 0 ) {
+			ulog('Warn: Clock moved backwards - Daylight Savings Time??');
+			srpm = 0;
+			slast = now;
+		} else if (srpm > argv.maxrpm ) {
+			ulog('Warn: throttling due to too many streaming requests per minute');
+			setTimeout(function() { 
+				tsla_poll( vid, long_vid, token );
+			}, 60000);	
+			return;
+		}	
+	} else { // longer than a minute since last request
+		srpm = 0;
+		slast = now;
+	}
+	srpm++; //increment the number of streaming requests per minute
+	request({'uri': s_url + long_vid +'/?values=' + argv.values,
 			'method' : 'GET',
-			'auth': {
-				'user': creds.username,
-				'pass': token
-			},
+			'auth': {'user': creds.username,'pass': token},
 			'timeout' : 125000 // a bit more than the expected 2 minute max long poll
-		},
-		function( error, response, body) {
-			if ( error ) { // HTTP Error
-				if (!argv.silent) { util.log( error ); }
-				// put short delay to avoid stack overflow
-				setTimeout(function() {
-					tsla_poll( vid, long_vid, token ); // poll again
-				}, 10000);
-			} else if (response.statusCode == 200) { // HTTP OK
-				if (!argv.silent)
-				{
-					if (body===undefined)
-					{
-						util.log('undefined');
-					}
-					else if (body===null)
-					{
-						util.log('null');
-					}
-					else
-					{
-						util.log(body);
-					}
-				}
+			}, function( error, response, body) {
+		if ( error ) { // HTTP Error
+			ulog( error );
+			// put short delay to avoid infinite recursive loop and stack overflow
+			setTimeout(function() {
 				tsla_poll( vid, long_vid, token ); // poll again
-			} else if ( response.statusCode == 401) { // HTTP AUTH Failed
-				if (!argv.silent) {
-					util.log('WARN: HTTP 401: Unauthorized - token has likely expired, getting a new one');
-				}
-				// put short delay to avoid getting cut off by Tesla
-				setTimeout(function() {
-					initstream();
-				}, 10000);
+			}, 1000);
+		} else if (response.statusCode == 200) { // HTTP OK
+			if (body===undefined) {
+				ulog('WARN: HTTP returned OK but body is undefined');
+			} else if (body===null) {
+				ulog('WARN: HTTP returned OK but body is null');
 			} else {
-				if (!argv.silent) {
-					util.log('Problem with request:');
-					util.log('	Response status code = ' + response.statusCode );
-					util.log('	Error code = ' + error);
-					util.log('Polling again...');
-				}
-				// put short delay to avoid stack overflow
-				setTimeout(function() {
-					tsla_poll( vid, long_vid, token ); // poll again
-				}, 10000);
+				ulog(body);
 			}
+			// put short delay to avoid infinite recursive loop and stack overflow
+			setTimeout(function() {
+				tsla_poll( vid, long_vid, token ); // poll again
+			}, 1000);
+		} else if ( response.statusCode == 401) { // HTTP AUTH Failed
+			ulog('WARN: HTTP 401: Unauthorized - token has likely expired, reinitializing');
+			setTimeout(function() {
+				initstream();
+			}, 1000);
+		} else { // all other unexpected responses
+			ulog('Unexpected problem with request:\n	Response status code = ' + response.statusCode + '	Error code = ' + error + '\n Polling again in 10 seconds...');
+			// put short delay to avoid infinite recursive loop and stack overflow
+			setTimeout(function() {
+				tsla_poll( vid, long_vid, token ); // poll again
+			}, 10000);
 		}
-		).on('data', function(data) {
-			var d, vals, i, record, doc;
-			if (argv.db) {
-				d = data.toString().trim();
-				vals = d.split(/[,\n\r]/);
-				for (i = 0; i < vals.length; i += nFields) {
-					record = vals.slice(i, nFields);
-					doc = { 'ts': +vals[i], 'record': record };
-					collectionS.insert(doc, { 'safe': true }, function(err,docs) {
-						if(err) util.log(err);
-					});
+	}).on('data', function(data) {
+		var d, vals, i, record, doc;
+		if (argv.db) {
+			d = data.toString().trim();
+			vals = d.split(/[,\n\r]/);
+			for (i = 0; i < vals.length; i += nFields) {
+				record = vals.slice(i, nFields);
+				doc = { 'ts': +vals[i], 'record': record };
+				collectionS.insert(doc, { 'safe': true }, function(err,docs) {
+					if(err) util.log(err);
+				});
 //					collectionS.find({ 'ts': +vals[i]}).toArray(function(err, exist){
 //						try {
 //							if (err || exist == null || exist.length == 0) { // only write entry if it doesn't already exist
@@ -154,19 +163,31 @@ function tsla_poll( vid, long_vid, token ) {
 //							console.dir(innerError);
 //						}
 //					});
-				}
-			} else {
-				stream.write(data);
 			}
-		});
-	}
+		} else {
+			stream.write(data);
+		}
+	});		
 }
 
 function getAux() {
-	// make absolutely sure we don't overwhelm the API
-	if (new Date().getTime() - getAux.lastTime < 30000)
-		return;
-
+    // make absolutely sure we don't overwhelm the API
+    now = new Date().getTime();
+    if ( now - last < 60000) { // last request was within the past minute
+    	ulog( 'getAux: ' + rpm + ' of ' + argv.maxrpm + ' REST requests since ' + last);
+    	if ( now - last < 0 ) {
+			ulog('Warn: Clock moved backwards - Daylight Savings Time??');
+			rpm = 0;
+			last = now;
+		} else if ( rpm > argv.maxrpm ) {
+			ulog ('Throttling Auxiliary REST requests due to too much REST activity');
+			return;
+		}
+	} else { // longer than a minute since last request
+		rpm = 0;
+		last = now;
+	}
+	rpm = rpm + 2; // increase REST request counter by 2 for following requests
 	teslams.get_charge_state( getAux.vid, function(data) {
 		var doc = { 'ts': new Date().getTime(), 'chargeState': data };
 		collectionA.insert(doc, { 'safe': true }, function(err,docs) {
@@ -184,7 +205,6 @@ function getAux() {
 			});
 		}
 	});
-	getAux.lastTime = new Date().getTime();
 }
 
 function storeVehicles(vehicles) {
@@ -192,8 +212,9 @@ function storeVehicles(vehicles) {
 	collectionA.insert(doc, { 'safe': true }, function (err, docs) {
 		if (err) console.dir(err);
 	});
+	rpm++; // increment REST request counter 
 	teslams.get_vehicle_state(vehicles.id, function(data) {
-		if (!argv.silent) console.log( util.inspect(data));
+		ulog( util.inspect(data));
 		doc = { 'ts': new Date().getTime(), 'vehicleState': data };
 		collectionA.insert(doc, { 'safe': true }, function (err, docs) {
 			if (err) console.dir(err);
@@ -207,47 +228,88 @@ function storeVehicles(vehicles) {
 function initdb(vehicles) {
 	storeVehicles(vehicles);
 	getAux.vid = vehicles.id;
-	getAux.lastTime = 0;
-	getAux();
+	// getAux();
 	setInterval(getAux, 60000); // also get non-streaming data every 60 seconds
 }
 
-function initstream() {
-	teslams.vehicles( { email: creds.username, password: creds.password }, function ( vehicles ) {
-		if (!argv.silent) { util.log( util.inspect( vehicles) ); }
-		if ( typeof vehicles == "undefined" || typeof vehicles.tokens == "undefined" || vehicles.tokens[0] == undefined ) {
-			if (!argv.silent) {
-				util.log('Warn: no tokens returned, calling wake_up then trying again');
-			}
-			teslams.wake_up( vehicles.id, function( resp ) {
-                // ignore the wake_up() response and try again and hope for vehicle data and valid tokens
-                // added a 10sec delay to avoid a tight repeating loop 
-                // such as in the case wake_up returns unusually quickly or with an error 
-				setTimeout(function() {
-				    initstream();
-				}, 10000); 
-			});
-		} else {
-			// initialize DB or CSV stream only once
-			if (firstTime)
-			{
-				firstTime = false;
+function ulog( string ) {
+	if (!argv.silent) {
+		util.log( string );
+	}
+}
 
-				if (argv.db) {
+function initstream() {
+    // make absolutely sure we don't overwhelm the API
+    now = new Date().getTime();
+    if ( now - last < 60000) { // last request was within the past minute
+    	ulog( rpm + ' of ' + argv.maxrpm + ' REST requests since ' + last);
+    	if ( now - last < 0 ) {
+			ulog('Warn: Clock moved backwards - Daylight Savings Time??');
+			rpm = 0;
+			last = now;
+		} else if (rpm > argv.maxrpm) { // throttle check
+        	util.log('Warn: throttling due to too many REST API');
+        	setTimeout(function() { 
+        		initstream(); 
+        	}, 60000); 
+        	return;
+        }       
+    } else { // longer than a minute since last request
+		last = now;
+		rpm = 0; // reset the REST API request counter
+	}
+	rpm++; // increment the REST API request counter
+	teslams.vehicles( { email: creds.username, password: creds.password }, function ( vehicles ) {	
+		if ( typeof vehicles == "undefined" ) {
+			console.log('Error: undefined response to vehicles request' );
+			console.log('Exiting...');
+			process.exit(1);
+		}
+		ulog( util.inspect( vehicles) ); 
+		if ( typeof vehicles.tokens == "undefined" || vehicles.tokens[0] == undefined ) {
+			ulog('Warn: no tokens returned');
+			if (argv.zzz && vehicles.state == 'asleep') { //respect sleep mode
+				ulog('Info: car is sleeping, will check again later');	
+                // wait for 5 minutes and check again if car is asleep
+				setTimeout(function() { 
+					initstream();
+				}, 300000);
+			} else { // car is either awake already OR don't care to let it sleep
+				ulog('Info: calling wake_up');
+				rpm++;	// increment the REST API request counter			
+				teslams.wake_up( vehicles.id, function( resp ) {
+					// check the wake_up() response and reinitialize to get vehicle data and valid tokens
+					// added a 30sec delay to avoid a tight repeating loop
+					// such as in the case wake_up returns unusually quickly or with an error 
+					if ( resp.result ) {
+						// wake_up returned true so re-initialize right away
+						setTimeout(function() { 
+							initstream(); 
+						}, 1000);
+					} else {
+						ulog('Warn: wake_up request failed.\n  Waiting 30 secs and then reinitializing...');
+						// wake_up failed. wait 30 seconds before trying again to reinitialize 
+						setTimeout(function() { 
+							initstream(); 
+						}, 30000);              
+					} 
+				});
+			}
+		} else { // this is the valid condition so we have the required tokens and ids
+			if (firstTime) {	// initialize only once
+				firstTime = false;
+				if (argv.db) { // initialize database
 					initdb(vehicles);
-				} else {
+				} else { // initialize first line of CSV file output with field names 
 					stream.write('timestamp,' + argv.values + '\n');
 				}
 			}
 			tsla_poll( vehicles.id, vehicles.vehicle_id, vehicles.tokens[0] );
 		}
-	});
+	}); // end of teslams.vehicles() request
 }
 
 // this is the main part of this program
-if (!argv.silent) {
-	util.log('timestamp,' + argv.values);
-}
-
 // call the REST API in order get login and get the id, vehicle_id, and streaming password token
+ulog('timestamp,' + argv.values);
 initstream();
