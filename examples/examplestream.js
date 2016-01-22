b7a79fd64cb608535af4f96e8aec3c9d1f2e98b1
Hans Jespersen <hans.jespersen@gmail.com>
2016-01-22 12:36:36 -0800

    fix simple examplestream.js app
    load credential from ~/.teslams/config.json so it will work like all
    the other examples.

--- examples/examplestream.js
+++ examples/examplestream.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 var teslams = require('../teslams.js');
 
-// edit the config.json file to contain your teslamotors.com login email and password
-var fs = require('fs');
-try {
+// edit the ~/.teslams/config.json file to contain your teslamotors.com login email and password
 
-	var jsonString = fs.readFileSync("./config.json").toString();
-	var config = JSON.parse(jsonString);
-	var creds = { 
-		email: config.username, 
-		password: config.password 
-	};
+try {
+	var configFile = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + "/.teslams/config.json";
+	var config = JSON.parse(require('fs').readFileSync(configFile).toString());
+	if ( config.hasOwnProperty('username') && config.hasOwnProperty('password') ) {
+		var creds = { 
+			email: config.username, 
+			password: config.password 
+		};
+	} else {
+		console.log("Unable to load " + configFile + "; username & password are required arguments");
+		console.log (config);
+		process.exit(1);		
+	}
 } catch (err) {
-	console.warn("The file 'config.json' does not exist or contains invalid arguments! Exiting...");
+	console.log("Error parsing config file: " + err);
 	process.exit(1);
 }
+
 
 //This is a very simple example to show the sequence required to call the stream() function
 //
@@ -25,16 +31,19 @@ try {
 //First you need to call vehicles() to get the vehicle_id and tokens to use as passwords 
 //Note that the password used for the REST call to vehicles() is different than 
 //   the password used in the streaming API call to stream() even though the email is the same
+
+console.log ('calling teslams.vehicles()');
 teslams.vehicles( { email: creds.email, password: creds.password }, function ( vehicles ) {
-    if (vehicles == undefined) {
+    console.log( 'got vehicles data');
+    if (typeof vehicles === "undefined") {
         console.log("Error: Undefined vehicle");
-    } else if (vehicles.tokens[0] == undefined) {
-        console.log("Error: Undefined token, call wakeup first using 'teslacmd -w'");
+    } else if (vehicles.tokens === undefined) {
+        console.log("Error: Undefined token, car might be sleeping, call wakeup first using 'teslacmd -w'");
     } else {
-	console.log('Calling Streaming API, expect to wait up to 2 minutes for full response...');
+	console.log('Calling Streaming API using token from vehicles response, expect to wait up to 2 minutes for full response...');
 	var params = { email: creds.email, password: vehicles.tokens[0], vehicle_id: vehicles.vehicle_id };
         teslams.stream( params, function (error, response, body) { 
-		console.log( body); 
-	});
+			console.log(body); 
+		});
     }
 });
