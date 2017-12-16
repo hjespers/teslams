fd68bc4a67b0f780757aaba91bdf724c7ce401ed
Drew Wells <drew.wells00@gmail.com>
2017-12-16 15:43:14 -0600

    support env variable for passing config.json location, fixes some examples where token was not supported

--- examples/config.js
+++ examples/config.js
@@ -1,94 +1,96 @@
 'use strict';
 /*
-	Try and find EITHER username and password OR token from command line arguments.
-	If not found, attempt to load it from ~/.teslams/config.json
+    Try and find EITHER username and password OR token from command line arguments.
+    If not found, attempt to load it from ~/.teslams/config.json
 
-	format of config.json:
+    format of config.json:
 
-	{
-		"username": "teslamotors.com username/email",
-		"password": "teslamotors.com password",
-		"token": "authorization token",
-		"visualize": {
-			"baseUrl": "/teslavis",
-			"webusers": [
-				{ "id": 1, "username": "dirk", "password": "secret" },
-				{ "id": 2, "username": "bob", "password": "different" }
-			]
-		}
-	}
+    {
+        "username": "teslamotors.com username/email",
+        "password": "teslamotors.com password",
+        "token": "authorization token",
+        "visualize": {
+            "baseUrl": "/teslavis",
+            "webusers": [
+                { "id": 1, "username": "dirk", "password": "secret" },
+                { "id": 2, "username": "bob", "password": "different" }
+            ]
+        }
+    }
 
-	If there is any trouble loading or parsing that file,
-	make the optimist args required as usual.
+    If there is any trouble loading or parsing that file,
+    make the optimist args required as usual.
 
-	This now also contains the web user/password database and path for visualize.js.
-	Please note that here all the properties need to be in quotes as we are parsing
-	this file as JSON - so don't just copy the array as it was in visualize.js before
-	(where id, username and password were NOT quoted).
+    This now also contains the web user/password database and path for visualize.js.
+    Please note that here all the properties need to be in quotes as we are parsing
+    this file as JSON - so don't just copy the array as it was in visualize.js before
+    (where id, username and password were NOT quoted).
 */
 
 exports.config = function (opt) {
-	var config,
-		configFile = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] +
-				"/.teslams/config.json",
-		configSuccess = false;
+    var config,
+        configFile = process.env.TSLA_CONFIG_FILE ||
+            process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] +
+                "/.teslams/config.json",
+        configSuccess = false;
 
-	if (opt.argv['$0'].indexOf("visualize.js") != -1) {
-		// we are calling from visualize.js, so let's get the user/password pairs for that
-		try {
-			config = JSON.parse(require('fs').readFileSync(configFile).toString());
-			configSuccess = config.hasOwnProperty('visualize');
-		} catch (err) {
-			console.warn("Unable to load " + configFile + "; web server authentication turned off");
-		}
-		if (configSuccess) {
-			if (opt.argv.debug) {
-				console.log("found " + config.visualize.webusers.length + " user / password entries; enabling web authentication");
-			}
-		} else {
-			console.log("didn't find 'visualize' property in config file, web authentication turned off");
-		}
-		return config;
-	}
-	// if no user name & password (or token) supplied on cmd line options, check environment variable
-	if (!opt.argv.username && !opt.argv.password && !opt.argv.token) {
-		config = {
-			username: process.env.TSLA_USERNAME,
-			password: process.env.TSLA_PASSWORD 
-		};
-		if (config.username != undefined && config.password != undefined) {
-			if (opt.argv.debug) {
-				console.log('Teslamotors.com logon information loaded from environment variables $TSLA_USERNAME and $TSLA_PASSWORD');
-			}
-			return config;
-		}
-	}
+    if (opt.argv['$0'].indexOf("visualize.js") != -1) {
+        // we are calling from visualize.js, so let's get the user/password pairs for that
+        try {
+            config = JSON.parse(require('fs').readFileSync(configFile).toString());
+            configSuccess = config.hasOwnProperty('visualize');
+        } catch (err) {
+            console.warn("Unable to load " + configFile + "; web server authentication turned off");
+        }
+        if (configSuccess) {
+            if (opt.argv.debug) {
+                console.log("found " + config.visualize.webusers.length + " user / password entries; enabling web authentication");
+            }
+        } else {
+            console.log("didn't find 'visualize' property in config file, web authentication turned off");
+        }
+        return config;
+    }
+    // if no user name & password (or token) supplied on cmd line options, check environment variable
+    if (!opt.argv.username && !opt.argv.password && !opt.argv.token) {
+        config = {
+            username: process.env.TSLA_USERNAME,
+            password: process.env.TSLA_PASSWORD
+        };
+        if (config.username != undefined && config.password != undefined) {
+            if (opt.argv.debug) {
+                console.log('Teslamotors.com logon information loaded from environment variables $TSLA_USERNAME and $TSLA_PASSWORD');
+            }
+            return config;
+        }
+    }
 
-	// if no user name & password (or token) supplied on cmd line options, look in config file
-	if (!opt.argv.username && !opt.argv.password && !opt.argv.token) {
-		try {
-			config = JSON.parse(require('fs').readFileSync(configFile).toString());
-			configSuccess = ( config.hasOwnProperty('username') && config.hasOwnProperty('password')) || config.hasOwnProperty('token');
-		} catch (err) {
-			console.warn("Unable to load " + configFile + "; username & password are required arguments");
-		}
-	}
-	if (configSuccess) {
-		if (opt.argv.debug) {
-			console.log('Teslamotors.com logon information loaded from ' + configFile);
-		}
-	} else if (opt.argv.token) {
-		config = { token: opt.argv.token };
-	} else {
-		// fall back to making them optimist required options
-		opt.demand('u');
-		opt.demand('p');
-		config =
-		{
-			username: opt.argv.username,
-			password: opt.argv.password,
-			token: opt.argv.token
-		};
-	}
-	return config;
+    // if no user name & password (or token) supplied on cmd line options, look in config file
+    if (!opt.argv.username && !opt.argv.password && !opt.argv.token) {
+        try {
+            config = JSON.parse(require('fs').readFileSync(configFile).toString());
+            configSuccess = ( config.hasOwnProperty('username') && config.hasOwnProperty('password')) || config.hasOwnProperty('token');
+        } catch (err) {
+            console.warn("Unable to load " + configFile + "; username & password are required arguments");
+        }
+    }
+    if (configSuccess) {
+        if (opt.argv.debug) {
+            console.log('Teslamotors.com logon information loaded from ' + configFile);
+        }
+    } else if (opt.argv.token) {
+        config = { token: opt.argv.token };
+    } else {
+        // fall back to making them optimist required options
+        opt.demand('u');
+        opt.demand('p');
+        config =
+        {
+            username: opt.argv.username,
+            password: opt.argv.password,
+            token: opt.argv.token
+        };
+    }
+
+    return config;
 };
--- examples/examplestream.js
+++ examples/examplestream.js
@@ -3,24 +3,10 @@ var teslams = require('../teslams.js');
 
 // edit the ~/.teslams/config.json file to contain your teslamotors.com login email and password
 
-try {
-	var configFile = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + "/.teslams/config.json";
-	var config = JSON.parse(require('fs').readFileSync(configFile).toString());
-	if ( config.hasOwnProperty('username') && config.hasOwnProperty('password') ) {
-		var creds = { 
-			email: config.username, 
-			password: config.password 
-		};
-	} else {
-		console.log("Unable to load " + configFile + "; username & password are required arguments");
-		console.log (config);
-		process.exit(1);		
-	}
-} catch (err) {
-	console.log("Error parsing config file: " + err);
-	process.exit(1);
-}
+var argv = require('optimist')
+    .usage('Usage: TSLA_CONFIG_FILE=.../config.json $0')
 
+var creds = require('./config.js').config(argv);
 
 //This is a very simple example to show the sequence required to call the stream() function
 //
@@ -28,22 +14,22 @@ try {
 //for data, expiring tokens, automatically wakes up the car when needed, handles sleep mode properly,
 //and writes the data to either a file or a mongodb database.
 //
-//First you need to call vehicles() to get the vehicle_id and tokens to use as passwords 
-//Note that the password used for the REST call to vehicles() is different than 
+//First you need to call vehicles() to get the vehicle_id and tokens to use as passwords
+//Note that the password used for the REST call to vehicles() is different than
 //   the password used in the streaming API call to stream() even though the email is the same
 
 console.log ('calling teslams.vehicles()');
-teslams.vehicles( { email: creds.email, password: creds.password }, function ( vehicles ) {
+teslams.vehicles( creds, function ( vehicles ) {
     console.log( 'got vehicles data');
     if (typeof vehicles === "undefined") {
         console.log("Error: Undefined vehicle");
     } else if (vehicles.tokens === undefined) {
         console.log("Error: Undefined token, car might be sleeping, call wakeup first using 'teslacmd -w'");
     } else {
-	console.log('Calling Streaming API using token from vehicles response, expect to wait up to 2 minutes for full response...');
-	var params = { email: creds.email, password: vehicles.tokens[0], vehicle_id: vehicles.vehicle_id };
-        teslams.stream( params, function (error, response, body) { 
-			console.log(body); 
-		});
+        console.log('Calling Streaming API using token from vehicles response, expect to wait up to 2 minutes for full response...');
+        var params = { email: creds.email, password: vehicles.tokens[0], vehicle_id: vehicles.vehicle_id };
+        teslams.stream( params, function (error, response, body) {
+            console.log(body);
+        });
     }
 });
